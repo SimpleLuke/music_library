41c3134a8fc44483f32f86436c5121e97301d0c1
Luke Lai <lukelai.dev@gmail.com>
2023-01-30 12:10:23 +0000

    Test drive the all method with database

---
+++ app.rb
@@ -0,0 +1,18 @@
+# file: app.rb
+require_relative 'lib/database_connection'
+require_relative './lib/artist_repository'
+
+# We need to give the database name to the method `connect`.
+DatabaseConnection.connect('music_library')
+
+# Perform a SQL query on the database and get the result set.
+# sql = 'SELECT id, title FROM albums;'
+# result = DatabaseConnection.exec_params(sql, [])
+
+# Print out each record from the result set .
+
+artist_repository = ArtistRepository.new
+
+artist_repository.all.each do |artist|
+  p artist
+end
